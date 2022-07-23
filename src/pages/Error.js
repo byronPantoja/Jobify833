2163041a840878a3d69a65dcb8b9700b4fef1500
byronPantoja <byronpantoja@icloud.com>
2022-07-23 17:28:08 +0800

    gitflow-feature-stash: registerPage

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -8,7 +8,7 @@ const Error = () => {
       <div>
         <img src={img} alt="not found" />
         <h3>Ohh! Page not found!</h3>
-        <p>We can't seem to find the page you're looking for</p>
+        <p>We can't seem to find the page you're looking for.</p>
         <Link to="/">back home</Link>
       </div>
     </Wrapper>
